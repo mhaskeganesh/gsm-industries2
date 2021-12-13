1a19a78d83f974b61c14bafeb7cfaf42f2a09bb9
Ganesh Mhaske <mhaskeganesh4545@gmail.com>
2021-12-13 10:12:48 +0530

    added

--- force-app/main/default/lwc/contactRecordForm/contactRecordForm.js
+++ force-app/main/default/lwc/contactRecordForm/contactRecordForm.js
@@ -3,8 +3,8 @@ import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
 export default class RecordFormExample extends LightningElement {
     // Expose a field to make it available in the template
-    fields = [NAME_FIELD];
-    objectApiName = CONTACT_OBJECT;
+   // fields = [NAME_FIELD];
+    //objectApiName = CONTACT_OBJECT;
     // Flexipage provides recordId and objectApiName
     //@api recordId;
     //@api objectApiName = 'Contact';
